97b0c0479ca74732dbee025d25d9d81f0fdaf89f
jluchett <jluchett@hotmail.com>
2024-10-15 14:49:43 -0500

    Agregado condicion de respuesta en controler para obtener items del carrito

--- src/controllers/cart_items/getCartItems.js
+++ src/controllers/cart_items/getCartItems.js
@@ -7,6 +7,9 @@ const getCartItems = async (req, res) => {
             'SELECT * FROM cart_items WHERE cart_id = $1',
             [cart_id]
         );
+        if(cartItems.rows.length === 0){
+           return res.status(404).json({status: "No hay items en el carrito"});
+        };
         res.json(cartItems.rows);
     } catch (err) {
         console.error(err.message);
